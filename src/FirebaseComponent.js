b4a377969b37b9bd15a142629459285b5dd488f1
Wojtek Pyda <w.pyda@poczta.onet.pl>
2018-01-21 13:51:53 +0100

    sending data to database

--- src/FirebaseComponent.js
+++ src/FirebaseComponent.js
@@ -1,14 +1,44 @@
 import React from 'react'
 import {database} from './firebase'
 
-class FirebaseComponent extends React.Component {
+import RaisedButton from 'material-ui/RaisedButton'
+import TextField from 'material-ui/TextField'
 
-    componentWillMount() {
-        console.log(database)
+class FirebaseComponent extends React.Component {
+    state = {
+        text: null
     }
+
+    setButtonHandler = () => {
+        database.ref('/setExample').set(this.state.text).then(() => alert('Zapisano dane metodą set'))
+    }
+
+    typeText = (event) => {
+        this.setState({text: event.target.value})
+    }
+
     render() {
         return (
-            <div>Firebase</div>
+            <div>
+                <h1>Firebase</h1>
+                <div>
+                    <TextField
+                        floatingLabelText="JSON to send"
+                        multiLine={true}
+                        rows={2}
+                        onChange={this.typeText}
+                    />
+                </div>
+
+                <div>
+                    <RaisedButton
+                        label="SET"
+                        secondary={true}
+                        onClick={this.setButtonHandler}
+                        fullWidth={true}
+                    />
+                </div>
+            </div>
         )
     }
 }
