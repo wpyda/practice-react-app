b3b1ffdbe88587576f45de89544fdb322ca9ca34
Wojtek Pyda <w.pyda@poczta.onet.pl>
2018-01-25 09:47:49 +0100

    action that indicates fetch status

--- src/state/asyncActions.js
+++ src/state/asyncActions.js
@@ -1,4 +1,7 @@
 const SET_DATA = 'asyncActions/SET_DATA'
+const FETCHING_STARTED = 'asyncActions/FETCHING_STARTED'
+const FETCHING_FINISHED = 'asyncActions/FETCHING_FINISHED'
+const FETCHING_FAILED = 'asyncActions/FETCHING_FAILED'
 
 // ACTION CREATOR
 export const setData = data => ({
@@ -6,14 +9,29 @@ export const setData = data => ({
     data: data
 })
 
+const fetchingStarted = () => ({type: FETCHING_STARTED})
+const fetchingFinished = () => ({type: FETCHING_FINISHED})
+const fetchingFailed = () => ({type: FETCHING_FAILED})
+
 export const fetchData = () => (dispatch, getState) => {
+    dispatch(fetchingStarted())
     fetch('https://randomuser.me/api')
         .then(response => response.json())
-        .then(data => dispatch(setData(data.results[0])))
+        .then(data => {
+            dispatch(setData(data.results[0]))
+            dispatch(fetchingFinished())
+        })
+        .catch(error => dispatch(fetchingFailed()))
 }
 
 const initialState = {
-    randomUserData: null
+    randomUserData: null,
+    messageForUser: ''
+}
+
+const messages = {
+    started: "Ładowanie",
+    failed: "Wystąpił błąd",
 }
 
 export default (state = initialState, action) => {
@@ -23,6 +41,21 @@ export default (state = initialState, action) => {
                 ...state,
                 randomUserData: action.data
             }
+        case FETCHING_STARTED:
+            return {
+                ...state,
+                messageForUser: messages.started
+            }
+        case FETCHING_FINISHED:
+            return {
+                ...state,
+                messageForUser: initialState.messageForUser // initialState.messageForUser to to samo co '', ale '' to duplikat messageForUser
+            }
+        case FETCHING_FAILED:
+                return {
+                    ...state,
+                    messageForUser: messages.failed
+                }
         default:
             return state
     }
