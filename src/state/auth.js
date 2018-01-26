a6b72253d4a88fb0b26014e53e0364acc7f6db4c
Wojtek Pyda <w.pyda@poczta.onet.pl>
2018-01-26 11:26:24 +0100

    login logs sync to state

--- src/state/auth.js
+++ src/state/auth.js
@@ -1,17 +1,43 @@
-import {auth, googleProvider} from '../firebase'
+import {database, auth, googleProvider} from '../firebase'
 
 const SET_USER = 'auth/SET_USER'
+const SET_LOGIN_LOGS = 'auth/SET_LOGIN_LOGS'
 
 const setUser = (user) => ({
     type: SET_USER,
     userData: user
 })
 
+const setLoginLogs = (logs) => ({
+    type: SET_LOGIN_LOGS,
+    logsData: logs
+})
+
 export const initAuth = () => (dispatch, getState) => {
     auth.onAuthStateChanged((user) => {
         // if not logged in, user is null
         dispatch(setUser(user))
+
+        if (user) { // check if not null
+            dispatch(logLoginDate())
+            dispatch(syncLoginLogs())
+        }
+
     })
+}
+
+const syncLoginLogs = () => (dispatch, getState) => {
+    const uid = getState().auth.user.uid
+    database.ref(`/users/${uid}/loginLogs`)
+        .on('value', (snapshot) => dispatch(setLoginLogs(snapshot.val())))
+}
+
+const logLoginDate = () => (dispatch, getState) => {
+    const uid = getState().auth.user.uid
+    database.ref(`/users/${uid}/loginLogs`)
+        .push({timestamp: Date.now()})
+        .then(() => console.log('Login date successfully logged in db'))
+        .catch(() => alert('Something wrong'))
 }
 
 export const logIn = (email, password) => (dispatch, getState) => {
@@ -42,6 +68,12 @@ export default (state = initialState, action) => {
             return {
                 ...state,
                 user: action.userData
+            }
+
+        case SET_LOGIN_LOGS:
+            return {
+                ...state,
+                loginLogs: action.logsData
             }
 
         default:
